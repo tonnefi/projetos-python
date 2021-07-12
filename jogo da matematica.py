143f07372ed85aa4e0a174c7123d96179742ebe2
tonnefi <87247191+tonnefi@users.noreply.github.com>
2021-07-11 22:41:41 -0300

    Add files via upload

---
+++ jogo da matematica.py
@@ -0,0 +1,29 @@
+#nm é uma infomação que você vai responder e ficara salva no cumputaodor!
+nm = str(input('qual é seu nome?'))
+
+#idd é outra informação que sera salva.
+idd = int(input('qual é sua idade?'))
+
+#pt1 vai... bom,salvar uma infomação(dnv kkk).
+pt1 = input('do que você gosta?')
+
+
+#aqui se colocar progamar vai dar uma resposta ja progamada!
+if pt1 == 'progamar':
+    print('uau eu também!!!kkkkk')
+
+#mas se não, diz outra mensagem!
+else:
+    print('hmm...Que legal!!!')
+
+#agora irei mostrar as infomações digitadas.
+print(f'você é {nm}, você tem {idd} anos, e você gosta de {pt1} certo?')
+
+#um adsinho padrão ;^)
+print('mano tenho canal no YouTube de jogos e futuramente progamação passa lá e se inscreve, só pesquisar Kenozos...vlw!!!')
+
+#agora vou dar um tchau!
+print(f'mano foi um prazer te conhecer {nm} espero muito nos encontrarmos novamente um dia!')
+
+#assinatura:
+print('\33[30;47;1;4mde: ~Gabriel, Kenozos e Tonnefi.\33[m')
