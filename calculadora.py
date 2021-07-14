26999d0bd2d556c319f2aef1261138eace890bd5
tonnefi <87247191+tonnefi@users.noreply.github.com>
2021-07-14 16:58:46 -0300

    Uma calculadora que faz 5 operações!!!
    Fiz uma calculadora em Python que tem as operações de: adição, subtração, multiplicação, divisão e divisão inteira. você pode fazer o dowload do arquivo e colocar a operação que quise!!!mas não se esqueça de deixar os creditos!!!

---
+++ calculadora.py
@@ -0,0 +1,15 @@
+//                                                                                    Kenozos
+#criando as variaveisque guardaram um valor digitados neles:
+num1 = float(input('Digite um número:'))
+num2 = float(input('Digite outro número:'))
+
+#resultado de cada operação:
+resultAd = num1 + num2
+resultSub = num1 - num2
+resultMult = num1 * num2
+resultDiv = num1 / num2
+resultDivInt= num1 // num2
+
+#e "printãndo" os resultados
+print(f'{num1} mais {num2} é igual a {resultAd}:\n{num1} menos {num2} é igual a {resultSub}:\n{num1} mulpitplicado por {num2} é igual a {resultMult}:\n{num1} divido por {num2} é igual a {resultDiv:.3}\n{num1} divido (numa divisão inteira) por {num2} é {resultDivInt}!')
+//                                                                                    Kenozos
